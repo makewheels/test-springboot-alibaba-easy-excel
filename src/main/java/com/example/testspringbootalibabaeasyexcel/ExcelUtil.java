a8d52409e1b6e7c396655453a798fe570cb39b43
makewheels <makewheels>
2021-04-05 11:52:23 +0800

    下载文件失败

--- src/main/java/com/example/testspringbootalibabaeasyexcel/ExcelUtil.java
+++ src/main/java/com/example/testspringbootalibabaeasyexcel/ExcelUtil.java
@@ -1,13 +1,16 @@
 package com.example.testspringbootalibabaeasyexcel;
 
 import com.alibaba.excel.EasyExcel;
+import com.alibaba.fastjson.JSON;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Excel工具类
@@ -35,8 +38,19 @@ public class ExcelUtil {
         }
         response.setHeader("Content-disposition",
                 "attachment;filename*=utf-8''" + fileName);
-        EasyExcel.write(response.getOutputStream(), clazz)
-                .sheet("Sheet1")
-                .doWrite(data);
+        try {
+            EasyExcel.write(response.getOutputStream(), clazz)
+                    .sheet("Sheet1")
+                    .doWrite(data);
+        } catch (Exception e) {
+            // 重置response
+            response.reset();
+            response.setContentType("application/json");
+            response.setCharacterEncoding("utf-8");
+            Map<String, String> map = new HashMap<>();
+            map.put("status", "failure");
+            map.put("message", "下载文件失败：" + e.getMessage());
+            response.getWriter().println(JSON.toJSONString(map));
+        }
     }
 }
